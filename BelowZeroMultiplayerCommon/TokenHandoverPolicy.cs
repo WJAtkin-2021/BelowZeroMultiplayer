fabff1f96cf4525564940cbc73bfff5ee58c65eb
William Atkin <w.j.atkin-2021@hull.ac.uk>
2024-10-27 11:52:18 +0000

    Started on TokenHandoverPolicy.cs

---
+++ BelowZeroMultiplayerCommon/TokenHandoverPolicy.cs
@@ -0,0 +1,10 @@
+﻿namespace BelowZeroMultiplayerCommon
+{
+    public enum TokenHandoverPolicy
+    {
+        Destroy = 0,
+        YieldTokenToOtherClients = 1,
+        AllowServerToYieldToken = 2,
+        NeverYieldToken = 3,
+    }
+}
